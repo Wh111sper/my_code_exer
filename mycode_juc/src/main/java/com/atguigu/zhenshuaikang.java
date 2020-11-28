cd8046099df96cf1f3108ce0d96669f6d99bf5ea
郑帅哥 <1608200596@qq.com>
2020-11-28 10:00:31 +0800

    220

---
+++ mycode_juc/src/main/java/com/atguigu/zhenshuaikang.java
@@ -0,0 +1,43 @@
+package com.atguigu;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author shkstart
+ * @create 2020-11-28 8:58:26
+ */
+public class zhenshuaikang {
+    public static void main(String[] args) {
+        final Object objectA = new Object();
+        final Object objectB = new Object();
+        new Thread(() -> {
+            synchronized (objectA){
+                System.out.println("线程A拿到objectA,想要控制objectB");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (objectB){
+                    System.out.println("A线程拿到objectB");
+                }
+            }
+        },"A").start();
+
+        new Thread(() -> {
+            synchronized (objectB){
+                System.out.println("线程B拿到objectB,想要控制objectA");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (objectA){
+                    System.out.println("B线程拿到objectA");
+                }
+            }
+        },"B").start();
+    }
+}
